bc98a195311b34815f2c0b04c813aab6a814a3f3
Jonathan Matthews <44363860+jonathanmatthews@users.noreply.github.com>
2022-06-07 19:49:19 +0100

    Add the stuff

---
+++ IyraObject.cs
@@ -0,0 +1,11 @@
+﻿namespace Iyra
+{
+    public abstract class IyraObject<T> where T : IyraObject<T>
+    {
+        protected readonly T self;
+        protected IyraObject()
+        {
+            self = (T)this;
+        }
+    }
+}
